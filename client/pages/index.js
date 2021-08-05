f1f8aee9404632e37f8324e76fa2da6dedeb5a64
rakshith <rakshit.s.gowda@gmail.com>
2021-08-05 17:12:21 +0530

    added bootstrap and ant design to the project

--- client/pages/index.js
+++ client/pages/index.js
@@ -1,7 +1,7 @@
 const Index = () => {
   return (
     <>
-      <h1>Hello world</h1>
+      <h1 className="bg-primary rounded-2 p-4 mb-2 text-center">Hello world</h1>
       <p>this is the paragraph component</p>
     </>
   )
