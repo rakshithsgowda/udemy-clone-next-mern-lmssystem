e76c80ee4de811723c8f4204995b95261caa9171
rakshith <rakshit.s.gowda@gmail.com>
2021-08-05 19:32:53 +0530

    registeration page detup

---
+++ client/pages/login.js
@@ -0,0 +1,10 @@
+const Login = () => {
+  return (
+    <>
+     <h1 className="bg-primary p-4 mb-2 text-center jumbotron">login page</h1>
+      <p>this is the this is login page</p>
+    </>
+  )
+}
+
+export default Login
