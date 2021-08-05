58427ce028fda9133843015349f39b2cfb2a5113
rakshith <rakshit.s.gowda@gmail.com>
2021-08-05 17:56:05 +0530

    added custom css

--- client/pages/_app.js
+++ client/pages/_app.js
@@ -1,9 +1,10 @@
 import 'bootstrap/dist/css/bootstrap.min.css'
 import 'antd/dist/antd.css';
 
+import '../public/css/styles.css'
 
 function MyApp({Component,pageProps}) {
-  return <Component{...pageProps}/>
+  return <Component{...pageProps} />
 }
 
 export default MyApp
