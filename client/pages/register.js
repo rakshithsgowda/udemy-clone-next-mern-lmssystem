945a9d63aab9c9a95d1fdeddcb1cb516a0ae706e
rakshith <rakshit.s.gowda@gmail.com>
2021-08-07 16:25:08 +0530

    loading spinner

--- client/pages/register.js
+++ client/pages/register.js
@@ -1,16 +1,19 @@
 import { useState } from 'react'
 import axios from 'axios'
 import { toast } from 'react-toastify'
+import { SyncOutlined } from '@ant-design/icons'
 
 const Register = () => {
   const [name, setName] = useState('india')
   const [email, setEmail] = useState('india@india.com')
   const [password, setPassword] = useState('india')
+  const [loading, setLoading] = useState(false)
 
   const handleSubmit = async (e) => {
     e.preventDefault()
     // console.table({name,email,password})
     try {
+      setLoading(true)
       const { data } = await axios.post(`http://localhost:8000/api/register`, {
         name,
         email,
@@ -18,8 +21,10 @@ const Register = () => {
       })
       // console.log('REGISTER RESPONSE', data)
       toast.success('registration successful. Please login')
+      setLoading(false)
     } catch (err) {
       toast.error(err.response.data)
+      setLoading(false)
     }
   }
   return (
@@ -60,8 +65,12 @@ const Register = () => {
             required
           />
 
-          <button className='col-12 btn btn-lg btn-dark' type='submit'>
-            Submit
+          <button
+            className='col-12 btn btn-lg btn-dark'
+            type='submit'
+            disabled={!name || !email || !password || loading}
+          >
+            {loading ? <SyncOutlined spin /> : 'Submit'}
           </button>
         </form>
       </div>
