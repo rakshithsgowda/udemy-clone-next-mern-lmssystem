3349262f8b3c73913805e0d766c377d3d067a4a5
rakshith <rakshit.s.gowda@gmail.com>
2021-10-19 13:39:25 +0530

    proteected page frontend

--- server/middlewares/index.js
+++ server/middlewares/index.js
@@ -1,7 +1,7 @@
 import expressJwt from 'express-jwt'
 
 export const requireSignIn = expressJwt({
-  getToken: () => req.cookies.token,
+  getToken: (req, res) => req.cookies.token,
   secret: process.env.JWT_SECRET,
   algorithms: ['HS256'],
 })
